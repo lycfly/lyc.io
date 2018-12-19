d87337d794cd3fed080464a6473fbcecdd21b9a4
lycfly <601753574@qq.com>
2018-12-19 19:58:52 +0800

    Form auto backup script's commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,11 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-// 压缩 public 目录 css
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
         .pipe(gulp.dest('./public'));
 });
-// 压缩 public 目录 html
 gulp.task('minify-html', function() {
   return gulp.src('./public/**/*.html')
     .pipe(htmlclean())
@@ -21,13 +19,11 @@ gulp.task('minify-html', function() {
     }))
     .pipe(gulp.dest('./public'))
 });
-// 压缩 public/js 目录 js
 gulp.task('minify-js', function() {
     return gulp.src('./public/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
-// 执行 gulp 命令时执行的任务
 gulp.task('default', [
     'minify-html','minify-css','minify-js'
 ]);
